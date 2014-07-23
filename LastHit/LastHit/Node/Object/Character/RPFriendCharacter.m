0a5a407695678c0c8f94c729d8cf9715085ca970
qixin1106 <63401364@qq.com>
2014-07-24 01:50:45 +0800

    12321321

--- LastHit/LastHit/Node/Object/Character/RPFriendCharacter.m
+++ LastHit/LastHit/Node/Object/Character/RPFriendCharacter.m
@@ -2,7 +2,7 @@
 //  RPFriendCharacter.m
 //  LastHit
 //
-//  Created by gm-iMac-iOS-03 on 14-7-22.
+//  Created by Qixin on 14-7-22.
 //  Copyright (c) 2014年 Research&PS. All rights reserved.
 //
 
--- LastHit/LastHit/Node/Object/RPBaseObjNode.m
+++ LastHit/LastHit/Node/Object/RPBaseObjNode.m
@@ -2,7 +2,7 @@
 //  RPBaseObjNode.m
 //  LastHit
 //
-//  Created by gm-iMac-iOS-03 on 14-7-22.
+//  Created by Qixin on 14-7-22.
 //  Copyright (c) 2014年 Research&PS. All rights reserved.
 //
 
