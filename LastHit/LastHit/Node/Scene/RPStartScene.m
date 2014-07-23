6ee67528af2f9003c7fb4d6067b6cfa7deabc6d6
Qixin <qixin1106@qq.com>
2014-07-23 15:35:42 +0800

    23

--- LastHit/LastHit/Node/Scene/RPStartScene.m
+++ LastHit/LastHit/Node/Scene/RPStartScene.m
@@ -19,12 +19,10 @@
         self.backgroundColor = [SKColor colorWithRed:0.15 green:0.15 blue:0.3 alpha:1.0];
         
         SKLabelNode *myLabel = [SKLabelNode labelNodeWithFontNamed:@"Chalkduster"];
-        
-        myLabel.text = @"Hello, World!";
+        myLabel.text = @"特别的小游戏,特别小的游戏";
         myLabel.fontSize = 30;
         myLabel.position = CGPointMake(CGRectGetMidX(self.frame),
                                        CGRectGetMidY(self.frame));
-        
         [self addChild:myLabel];
     }
     return self;
