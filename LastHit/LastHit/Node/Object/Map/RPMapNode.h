80e7259ea993155df84ddceff9f5f5dc041a94ae
qixin1106 <63401364@qq.com>
2014-07-25 01:04:26 +0800

    212132

---
+++ LastHit/LastHit/Node/Object/Map/RPMapNode.h
@@ -0,0 +1,13 @@
+//
+//  RPMapNode.h
+//  LastHit
+//
+//  Created by 亓鑫 on 14-7-25.
+//  Copyright (c) 2014年 Research&PS. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+
+@interface RPMapNode : SKSpriteNode
+
+@end
---
+++ LastHit/LastHit/Node/Object/Map/RPMapNode.m
@@ -0,0 +1,13 @@
+//
+//  RPMapNode.m
+//  LastHit
+//
+//  Created by 亓鑫 on 14-7-25.
+//  Copyright (c) 2014年 Research&PS. All rights reserved.
+//
+
+#import "RPMapNode.h"
+
+@implementation RPMapNode
+
+@end
